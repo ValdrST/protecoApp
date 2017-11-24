e023ffee39fe879d1b1264bfca16ba9e184ac8f0
Valdr Stiglitz <valdr.stiglitz@gmail.com>
2017-11-24 00:27:09 -0600

    app

---
+++ Ap.Proteco/app/src/main/java/com/example/cesos/approteco/Unetenos.java
@@ -0,0 +1,15 @@
+package com.example.cesos.approteco;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Cesos on 23/11/2017.
+ */
+
+public class Unetenos extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unetenos);
+    }
+}
---
+++ Ap.Proteco/app/src/main/java/com/example/cesos/approteco/facebookActivity.java
@@ -0,0 +1,8 @@
+package com.example.cesos.approteco;
+
+/**
+ * Created by valdr on 24/11/17.
+ */
+
+public class facebookActivity {
+}
